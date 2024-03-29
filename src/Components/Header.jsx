d5468d32a195b1c9aafb1084a9db7b8f59f0c6db
Shafayet-tuhin <shafayettuhin00@gmail.com>
2024-03-30 05:06:49 +0600

    add daizy

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -16,9 +16,17 @@ const Header = () => {
 
       <div className="navbar px-4 py-4 bg-[#F3F3F3] rounded-2xl mt-7">
         <button className='btn btn-primary mr-7'>Latest</button>
-        <Marquee speed={50} gradien={true} gradient={true} gradientColor={['rgba(248, 251, 253, 1)']}  >
+        <Marquee className='mr-7' speed={50} gradien={true} gradient={true} gradientColor={['rgba(248, 251, 253, 1)']}  >
           Match Highlights: Germany vs Spain — as it happened   !   Match Highlights: Germany vs Spain as...
         </Marquee>
+
+        <button>
+          <label className="flex cursor-pointer gap-2">
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="5" /><path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" /></svg>
+            <input type="checkbox" value="synthwave" className="toggle theme-controller" />
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
+          </label>
+        </button>
       </div>
 
 
@@ -29,20 +37,20 @@ const Header = () => {
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
             </div>
             <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-             
+
             </ul>
           </div>
-         
+
         </div>
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1 flex gap-10">
-             <li>Home</li>
-             <li>About</li>
-             <li>Career</li>
+            <li>Home</li>
+            <li>About</li>
+            <li>Career</li>
           </ul>
         </div>
         <div className="navbar-end">
-        <img className="w-10 h-10 mr-2" src={user} alt="User" />
+          <img className="w-10 h-10 mr-2" src={user} alt="User" />
           <a className="btn">Login</a>
         </div>
       </div>
--- src/Components/RightNav.jsx
+++ src/Components/RightNav.jsx
@@ -1,25 +1,36 @@
-import React from 'react'
-
-import { FaFacebookF, FaGithub, FaGoogle } from "react-icons/fa";
+import React from "react";
+import { FaFacebookF, FaGithub, FaGoogle, FaInstagram, FaTwitter, FaTwitterSquare } from "react-icons/fa";
 const RightNav = () => {
   return (
     <div>
-      <h3>Login With</h3>
-      <button className='flex mb-2 items-center gap-2 border px-10 py-2 rounded-lg border-blue-500 hover:bg-blue-500 hover:text-white'><FaGoogle />Login with Google</button>
-      <button className='flex mb-2 items-center gap-2 border px-10 py-2 rounded-lg border-gray-500 hover:bg-gray-800 hover:text-white'><FaGithub />Login with GitHub</button>
 
-      <div>
-        <h3>Find Us On</h3>
-
-       
-
-<ul>
-  li
-</ul>
+      <div >
+        <h2 className="text-xl font-semibold leading-7">Login With</h2>
+        <button className="btn btn-outline btn-primary mt-4 mb-2 w-full">
+          <FaGoogle />
+          Login with Google
+        </button>
+        <button className="btn btn-outline w-full">
+          <FaGithub />
+          Login with GitHub
+        </button>
       </div>
 
-    </div>
-  )
-}
 
-export default RightNav
+         <div>
+          <p className="font-semibold text-xl leading-7 mt-7 mb-2">Find Us On</p>
+
+          <div>
+          <ul class="menu rounded-box">
+            <li><button className="py-4 px-14 border border-gray-200"> <FaFacebookF/> Facebook</button></li>
+            <li><button className="py-4 px-14 border border-gray-200"><FaTwitter/>Twitter</button></li>
+            <li><button className="py-4 px-14 border border-gray-200"><FaInstagram />Instagram</button></li>
+          </ul>
+          </div>
+         </div>
+
+    </div>
+  );
+};
+
+export default RightNav;
