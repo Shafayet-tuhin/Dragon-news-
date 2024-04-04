4a70cf2d3cdef49682577c38c06eaafece778193
Shafayet-tuhin <shafayettuhin00@gmail.com>
2024-04-04 23:27:22 +0600

    ok

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -3,16 +3,16 @@ import MainAbout from "./MainAbout";
 import LeftNav from "./LeftNav";
 import RightNav from "./RightNav";
 import Header from "./Header";
-
+import Footer from "./Footer";
 const About = () => {
     return (
-        <div>
+        <div className="container mx-auto">
             <Header />
-            <div className="lg:hidden">
+            <div className="lg:hidden mb-16">
                 <MainAbout />
             </div>
 
-            <div className="hidden lg:grid lg:grid-cols-12 lg:gap-4">
+            <div className="hidden lg:grid lg:grid-cols-12 lg:gap-4 ">
                 <div className="lg:col-span-3">
                     <LeftNav />
                 </div>
@@ -23,6 +23,7 @@ const About = () => {
                     <RightNav />
                 </div>
             </div>
+            <Footer/>
         </div>
     );
 };
